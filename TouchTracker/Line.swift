3405a9e390c5101eb0f586895561560012b73dad
steveharski <steveharski@gmail.com>
2017-11-20 12:31:32 +0200

    Chapter 18: Touch Events

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Adminaccount on 11/20/17.
+//  Copyright © 2017 Adminaccount. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
